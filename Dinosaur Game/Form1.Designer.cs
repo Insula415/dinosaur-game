50461c7c6149ca0c0d8642ca839c0640bbac6476
Insula <66838181+Insula415@users.noreply.github.com>
2021-01-23 15:20:14 +0000

    Add files via upload

---
+++ Dinosaur Game/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Dinosaur_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Floor = new System.Windows.Forms.PictureBox();
+            this.Trex = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Floor
+            // 
+            this.Floor.BackColor = System.Drawing.Color.Black;
+            this.Floor.Location = new System.Drawing.Point(-12, 452);
+            this.Floor.Name = "Floor";
+            this.Floor.Size = new System.Drawing.Size(652, 50);
+            this.Floor.TabIndex = 0;
+            this.Floor.TabStop = false;
+            this.Floor.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Trex
+            // 
+            this.Trex.Image = global::Dinosaur_Game.Properties.Resources.running;
+            this.Trex.Location = new System.Drawing.Point(131, 385);
+            this.Trex.Name = "Trex";
+            this.Trex.Size = new System.Drawing.Size(44, 60);
+            this.Trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Trex.TabIndex = 1;
+            this.Trex.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.obstacle1.Image = global::Dinosaur_Game.Properties.Resources.cactus1;
+            this.obstacle1.Location = new System.Drawing.Point(371, 405);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(27, 50);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle1.TabIndex = 1;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            this.obstacle1.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::Dinosaur_Game.Properties.Resources.cactus2;
+            this.obstacle2.Location = new System.Drawing.Point(534, 400);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(50, 50);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 1;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(9, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(106, 24);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.Floor);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.Trex);
+            this.Controls.Add(this.obstacle2);
+            this.Name = "Form1";
+            this.Text = "Dinosaur Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Floor;
+        private System.Windows.Forms.PictureBox Trex;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Dinosaur Game/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dinosaur_Game
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpSpeed = 10;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        readonly Random rand = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void GameEvent(object sender, EventArgs e)
+        {
+            Trex.Top += jumpSpeed;
+
+            txtScore.Text = "Score: " + score;
+
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if (x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 800);
+                        score++;
+                    }
+
+                    if (Trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        Trex.Image = Properties.Resources.dead;
+                        txtScore.Text += "  Press R to restart";
+                    }
+                }
+            }
+            if (Trex.Top >= 380 && !jumping)
+            {
+                // then we do the following
+                force = 12; // set the force to 8
+                Trex.Top = Floor.Top - Trex.Height; // also we place the player on top of the picture box
+                jumpSpeed = 0; // set the jump speed to 0
+            }
+
+            // if score is equals or greater than 10
+            if (score >= 10)
+            {
+                // the obstacle speed change to 15
+                obstacleSpeed = 15;
+            }
+        }
+        public void resetGame()
+        {
+            // This is the reset function
+            force = 12; // set the force to 8
+            Trex.Top = Floor.Top - Trex.Height; // also we place the player on top of the picture box
+            jumpSpeed = 0; // set the jump speed to 0
+            jumping = false; // change jumping to false
+            score = 0; // set score to 0
+            obstacleSpeed = 10; // set obstacle speed back to 10
+            txtScore.Text = "Score: " + score; // change the score text to just show the score
+            Trex.Image = Properties.Resources.running; // change the t rex image to running
+
+            foreach (Control x in this.Controls)
+            {
+                // is X is a picture box and it has a tag of obstacle
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    // generate a random number in the position integer between 600 and 1000
+                    int position = rand.Next(600, 1000);
+
+                    // change the obstacles left position to a random location begining of the game
+                    x.Left = 640 + (x.Left + position + x.Width * 3);
+                }
+            }
+            gameTimer.Start(); // start the timer
+        }
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+    }
+}
