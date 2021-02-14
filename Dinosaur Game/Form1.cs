2f75fca94394e8181464ebac8f501d932868ce05
Insula <66838181+Insula415@users.noreply.github.com>
2021-02-14 01:30:08 +0000

    Update Form1.cs

--- Dinosaur Game/Form1.cs
+++ Dinosaur Game/Form1.cs
@@ -85,44 +85,42 @@ namespace Dinosaur_Game
             }
             if (Trex.Top >= 380 && !jumping)
             {
-                // then we do the following
-                force = 12; // set the force to 8
-                Trex.Top = Floor.Top - Trex.Height; // also we place the player on top of the picture box
-                jumpSpeed = 0; // set the jump speed to 0
+                
+                force = 12; 
+                Trex.Top = Floor.Top - Trex.Height; 
+                jumpSpeed = 0;
             }
 
-            // if score is equals or greater than 10
+            
             if (score >= 10)
             {
-                // the obstacle speed change to 15
                 obstacleSpeed = 15;
             }
         }
         public void resetGame()
         {
-            // This is the reset function
-            force = 12; // set the force to 8
-            Trex.Top = Floor.Top - Trex.Height; // also we place the player on top of the picture box
-            jumpSpeed = 0; // set the jump speed to 0
-            jumping = false; // change jumping to false
-            score = 0; // set score to 0
-            obstacleSpeed = 10; // set obstacle speed back to 10
-            txtScore.Text = "Score: " + score; // change the score text to just show the score
-            Trex.Image = Properties.Resources.running; // change the t rex image to running
+           
+            force = 12; 
+            Trex.Top = Floor.Top - Trex.Height; 
+            jumpSpeed = 0; 
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10; 
+            txtScore.Text = "Score: " + score; 
+            Trex.Image = Properties.Resources.running; 
 
             foreach (Control x in this.Controls)
             {
-                // is X is a picture box and it has a tag of obstacle
+                
                 if (x is PictureBox && (string)x.Tag == "obstacle")
                 {
-                    // generate a random number in the position integer between 600 and 1000
+                    
                     int position = rand.Next(600, 1000);
-
-                    // change the obstacles left position to a random location begining of the game
+                    
                     x.Left = 640 + (x.Left + position + x.Width * 3);
                 }
             }
-            gameTimer.Start(); // start the timer
+            gameTimer.Start();
         }
         private void keyisdown(object sender, KeyEventArgs e)
         {
